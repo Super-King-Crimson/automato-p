478864e934c8fe8aeaf4166556f6fd429820b64a
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-08-18 19:46:56 -0400

    WTH FILES ARE BUSTEDD????

--- src/save_load.rs
+++ src/save_load.rs
@@ -1,28 +1,43 @@
-use std::{fs, io::{Write, BufWriter, BufRead, BufReader}, path::Path};
+use std::{fs::{self, File, OpenOptions}, io::{Error, Write, BufWriter, BufRead, BufReader}, path::Path};
 use crate::schedule::Schedule;
 
-pub struct SaveLoadPath(Path);
+pub struct SaveLoad {
+    path: Path
+}
 
-fn read_schedules_from_file(path: SaveLoadPath) -> Vec<Schedule> {
+impl SaveLoad {
+    fn read_all_schedules(&self) {
+        let file = OpenOptions::new().read(true).open(self.path).unwrap();
+    }
+
+    pub fn try_new(path: Path) -> Result<SaveLoad, Error> {
+        match path.try_exists() {
+            Ok(_) => Ok(SaveLoad { path }),
+            Err(e) => e,
+        }
+    }
+}
+
+fn read_schedules_from_file(path: SaveLoad) -> Vec<Schedule> {
     let file = fs::File::open(path).unwrap();
     let reader = BufReader::new(file);
 
     reader.lines().map(|line| serde_json::from_str(&line.unwrap()).unwrap()).collect()
 }
 
-fn write_schedule_to_file(data: &Schedule) {
+fn write_schedule_to_file(data: &Schedule, path: SaveLoadPath) {
     let mut json = serde_json::to_string(data).unwrap();
     json.push('\n');
 
-    let file = fs::OpenOptions::new().write(true).append(true).open(PATH_TO_FILE).unwrap();
+    let file = fs::OpenOptions::new().write(true).append(true).open(SCHEDULE_FILE).unwrap();
     let mut file = BufWriter::new(file);
 
     file.write_all(json.as_bytes()).unwrap();
     file.flush().unwrap();
 }
 
-pub fn delete_schedule(index: usize) {
-    let file = fs::OpenOptions::new().read(true).write(true).open(PATH_TO_FILE).unwrap();
+pub fn delete_schedule(index: usize, path: SaveLoadPath) {
+    let file = fs::OpenOptions::new().read(true).write(true).open(path).unwrap();
     let reader = BufReader::new(&file);
     
     let mut new_contents = String::new();
@@ -38,7 +53,7 @@ pub fn delete_schedule(index: usize) {
     writer.flush().unwrap();
 }
 
-pub fn replace_schedule(index: usize, replacement: &Schedule) {
+pub fn replace_schedule(index: usize, replacement: &Schedule, path: SaveLoadPath) {
     let file = fs::OpenOptions::new().read(true).write(true).open(PATH_TO_FILE).unwrap();
     let reader = BufReader::new(&file);
     
