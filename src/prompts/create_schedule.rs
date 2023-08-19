e441fb1583086da165566b05035d407dff5b6e4f
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-08-19 10:09:55 -0400

    Refactored create schedule to accept a string time to write unit tests

--- src/prompts/create_schedule.rs
+++ src/prompts/create_schedule.rs
@@ -1,31 +1,38 @@
 use crate::{
     app::ScheduleList,
-    utils::console, 
-    schedule::{Schedule, format, RepeatType, RestType},
+    schedule::{format, RepeatType, RestType, Schedule},
+    utils::console,
 };
+
+enum RepeatTypeResponse {
+    Infinite,
+    Finite { blocks: String },
+}
+
+enum RestTypeResponse {
+    Standard,
+    LongRest {
+        blocks_per_long_rest: String,
+        long_rest_duration: String,
+    },
+}
 
 struct ScheduleCreateResponses {
     name: String,
     work_duration: String,
     rest_duration: String,
-    repeat_type: (String, String),
-    rest_type: (String, String, String),
+    repeat_type: RepeatTypeResponse,
+    rest_type: RestTypeResponse,
 }
 
 impl ScheduleCreateResponses {
-    const REPEAT_INFINITE: String = String::from("Infinite");
-    const REPEAT_FINITE: String = String::from("Finite");
-
-    const LONG_REST: String = String::from("Long Rest");
-    const STANDARD_REST: String = String::from("No Long Rest");
-
     fn new() -> ScheduleCreateResponses {
-        ScheduleCreateResponses { 
+        ScheduleCreateResponses {
             name: String::new(),
             work_duration: String::new(),
             rest_duration: String::new(),
-            repeat_type: (String::new(), String::new()),
-            rest_type: (String::new(), String::new(), String::new()),
+            repeat_type: RepeatTypeResponse::Infinite,
+            rest_type: RestTypeResponse::Standard,
         }
     }
 }
@@ -43,86 +50,97 @@ const SCHEDULE_QUESTIONS: [&str; 8] = [
     "How long should your break be? (HH:MM:SS)",
 ];
 
-fn prompt(schedule_list: &mut ScheduleList) -> Option<ScheduleCreateResponses> {
+fn prompt() -> Option<ScheduleCreateResponses> {
     let mut responses = ScheduleCreateResponses::new();
 
-    let mut i = 0;
-    while i < SCHEDULE_QUESTIONS.len() {
+    let mut question_index = 0;
+    let mut previous_questions: Vec<usize> = Vec::new();
+    while question_index < SCHEDULE_QUESTIONS.len() {
         console::clear();
 
-        println!("{} (type 'BACK' at any point to go to previous question/menu)", SCHEDULE_QUESTIONS[i]);
+        println!(
+            "{} (type 'BACK' at any point to go to previous question/menu)",
+            SCHEDULE_QUESTIONS[question_index]
+        );
         let response = console::get_input_trimmed();
 
         if response.eq_ignore_ascii_case("back") {
-            if i == 0 {
-                return None;
-            } else {
-                i -= 1;
-                continue;
+            match previous_questions.pop() {
+                Some(prev) => {
+                    question_index = prev;
+                    continue;
+                }
+                None => return None,
             }
         }
 
-        match i {
+        previous_questions.push(question_index);
+
+        match question_index {
             0 => responses.name = response,
             1 => responses.work_duration = response,
             2 => responses.rest_duration = response,
-            3 => {
-                if response.eq("1") {
-                    responses.repeat_type.0 = ScheduleCreateResponses::REPEAT_FINITE.clone();
-                } else if response.eq("2") {
-                    responses.repeat_type.0 = ScheduleCreateResponses::REPEAT_INFINITE.clone();
-                    i += 1;
-                } else {
-                    panic!("Invalid response: must be 1 or 2");
+            3 => if response.eq("1") {
+                responses.repeat_type = RepeatTypeResponse::Finite {
+                    blocks: String::new(),
                 }
+            } else if response.eq("2") {
+                responses.repeat_type = RepeatTypeResponse::Infinite;
+                question_index += 1;
+            } else {
+                panic!("Invalid response: must be 1 or 2");
+            },
+            4 => if let RepeatTypeResponse::Finite { blocks } = &mut responses.repeat_type {
+                *blocks = response;
+            },
+            5 => if response.eq_ignore_ascii_case("y") {
+                responses.rest_type = RestTypeResponse::LongRest { blocks_per_long_rest: String::new(), long_rest_duration: String::new() }
+            } else if response.eq_ignore_ascii_case("n") {
+                responses.rest_type = RestTypeResponse::Standard;
+                question_index += 2;
+            } else {
+                panic!("Invalid response: y or n");
+            },
+            6 => if let RestTypeResponse::LongRest { blocks_per_long_rest, ..} = &mut responses.rest_type {
+                *blocks_per_long_rest = response;
             }
-            4 => responses.repeat_type.1 = response,
-            5 => {
-                if response.eq_ignore_ascii_case("y") {
-                    responses.rest_type.0 = ScheduleCreateResponses::LONG_REST;
-                } else if response.eq_ignore_ascii_case("n") {
-                    responses.repeat_type.0 = ScheduleCreateResponses::STANDARD_REST;
-                    i += 2;
-                } else {
-                    panic!("Invalid response: y or n");
-                }
+            7 => if let RestTypeResponse::LongRest { long_rest_duration, .. } = &mut responses.rest_type {
+                *long_rest_duration = response;
             }
-            6 => responses.rest_type.1 = response,
-            7 => responses.rest_type.2 = response,
-            _ => panic!("How did we get here?"),
+            _ => panic!("How Did You Do This"),
         }
 
-        i += 1;
+        question_index += 1;
     }
 
     Some(responses)
 }
 
-//all ScheduleCreateResponses should be able to be converted into a Schedule
-fn convert_to_schedule(responses: &ScheduleCreateResponses) -> Schedule {
+fn convert_to_schedule(responses: ScheduleCreateResponses) -> Schedule {
     Schedule {
         name: responses.name,
         work_duration: format::hhmmss_to_dur(&responses.work_duration),
         rest_duration: format::hhmmss_to_dur(&responses.rest_duration),
-        repeat_type: match &responses.repeat_type.0 {
-            &ScheduleCreateResponses::REPEAT_FINITE => RepeatType::Finite(responses.repeat_type.1.parse().expect("repeat_type.1 should be parseable to u32")),
-            &ScheduleCreateResponses::REPEAT_INFINITE => RepeatType::Infinite,
-            other => panic!("Could not convert '{other}' to Schedule repeat type"),
+        repeat_type: match responses.repeat_type {
+            RepeatTypeResponse::Finite { blocks } => RepeatType::Finite(blocks.parse().unwrap()),
+            RepeatTypeResponse::Infinite => RepeatType::Infinite,
         },
-        rest_type: match &responses.rest_type.0 {
-            &ScheduleCreateResponses::LONG_REST => RestType::LongRest {
-                blocks_per_long_rest: responses.rest_type.1.parse().unwrap(),
-                long_rest_duration: format::hhmmss_to_dur(&responses.rest_type.2),
+        rest_type: match responses.rest_type {
+            RestTypeResponse::LongRest {
+                blocks_per_long_rest: blocks,
+                long_rest_duration: dur,
+            } => RestType::LongRest {
+                blocks_per_long_rest: blocks.parse().unwrap(),
+                long_rest_duration: format::hhmmss_to_dur(&dur),
             },
-            &ScheduleCreateResponses::STANDARD_REST => RestType::Standard,
-            other => panic!("Could not convert '{other}' to Schedule rest type"),
-        }
+            RestTypeResponse::Standard => RestType::Standard,
+        },
     }
 }
 
-pub fn start(schedule_list: &mut ScheduleList) -> Option<Schedule> {
-    match prompt(schedule_list) {
-        Some(responses) => Some(schedule_list.push(convert_to_schedule(&responses))),
+pub fn start(schedule_list: &mut ScheduleList) -> Option<()> {
+    match prompt() {
+        Some(responses) => Some(schedule_list.push(convert_to_schedule(responses))),
         None => None,
     }
 }
@@ -135,16 +153,6 @@ mod tests {
 
     #[test]
     fn schedules_should_save_to_file() {
-        let list = ScheduleList::new();
-
-        let responses = ScheduleCreateResponses {
-            name: String::from("Test"),
-            work_duration: String::from("60:0"), 
-            rest_duration: String::from("0:1"),
-            repeat_type: (String::from(ScheduleCreateResponses::REPEAT_INFINITE), String::new()), 
-            rest_type: (ScheduleCreateResponses::LONG_REST, String::from("8"), String::from("2:0:0")),
-        };
-
-        
+        let _list = ScheduleCreateResponses::new();
     }
 }
